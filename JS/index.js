4e929b01bd978ab8078b3a43ad1ca0d6f35a094f
tiffany <tiffany.pelkey@uvm.edu>
2024-10-21 11:36:01 -0400

    commit1
    commit

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+function generateMadLibs() {
+const typeOfBird = document.getElementById ("typeOfBird");
+const typeOfBird = Raven;
+const roommateName = document.getElementById ("roommateName");
+const roommateName = Dracula;
+const noun = document.getElementById("noun");
+const noun = drink;
+const liquid = document.getElementById("liquid");
+const liquid = blood;
+const bodyPart = document.getElementById("bodyPart");
+const bodyPart = stomach;
+
+const madLibsText = `It was a cold October day. I woke to the smell of ${typeOfBird} roasting in the kitchen. My roommate said, "see if ${roommateName} needs a fresh ${noun}. So I carried a glass of ${liquid} into ${roommateName}'s room.  When I got there, I couldn't believe my ${bodyPart}!`;
+}
