d8a92fce69328960609aba6e0c682930939d63ac
tiffany <tiffany.pelkey@uvm.edu>
2024-10-23 08:47:41 -0400

    commit 2

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,9 @@
 function generateMadLibs() {
-const typeOfBird = document.getElementById ("typeOfBird");
-const typeOfBird = Raven;
-const roommateName = document.getElementById ("roommateName");
-const roommateName = Dracula;
-const noun = document.getElementById("noun");
-const noun = drink;
-const liquid = document.getElementById("liquid");
-const liquid = blood;
-const bodyPart = document.getElementById("bodyPart");
-const bodyPart = stomach;
-
+const typeOfBird = document.getElementById("typeOfBird").value;
+const roommateName = document.getElementById("roommateName").value;
+const noun = document.getElementById("noun").value;
+const liquid = document.getElementById("liquid").value;
+const bodyPart = document.getElementById("bodyPart").value;
 const madLibsText = `It was a cold October day. I woke to the smell of ${typeOfBird} roasting in the kitchen. My roommate said, "see if ${roommateName} needs a fresh ${noun}. So I carried a glass of ${liquid} into ${roommateName}'s room.  When I got there, I couldn't believe my ${bodyPart}!`;
+document.getElementById("mad-libs").textContent = madLibsText;
 }
